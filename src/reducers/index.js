452636011412923ea503c0f5a325ed09440eb863
Huazhi-Fang <hzfang80@gmail.com>
2020-07-27 16:07:14 -0400

    completed

---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+import { combineReducers } from 'redux'
+import authedUser from '../reducers/authedUser'
+import users from '../reducers/users'
+import tweets from '../reducers/tweets'
+import { loadingBarReducer } from 'react-redux-loading'
+
+
+export default combineReducers({
+    authedUser,
+    users,
+    tweets,
+    loadingBar: loadingBarReducer,
+})
